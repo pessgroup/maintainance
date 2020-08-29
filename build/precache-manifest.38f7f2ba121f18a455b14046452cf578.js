e1818e4078ed2544470d5ec747bb741126662f50
Bernard Ojengwa <bernardojengwa@gmail.com>
2020-08-29 09:32:49 +0100

    set landing page

---
+++ build/precache-manifest.38f7f2ba121f18a455b14046452cf578.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f91271f9525e430feabf7aa51a48b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcd8467ea9ee27f02759",
+    "url": "/static/css/main.87987450.chunk.css"
+  },
+  {
+    "revision": "baeafbce29309251f672",
+    "url": "/static/js/2.fd99430d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fd99430d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcd8467ea9ee27f02759",
+    "url": "/static/js/main.be381b48.chunk.js"
+  },
+  {
+    "revision": "3492a7be3e6ed6e3a5c2",
+    "url": "/static/js/runtime-main.aab64622.js"
+  },
+  {
+    "revision": "0c1ea6ced6dbffeebf3e5556c3e50493",
+    "url": "/static/media/support-maintenance.0c1ea6ce.png"
+  }
+]);
--- src/components/Maintenance.js
+++ src/components/Maintenance.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import './style.css'
-import support from './assets/support-maintenance.png'
+import './style.css';
+
+import React from 'react';
+
+import support from './assets/support-maintenance.png';
 
 function Maintenance() {
     return (
         <div>
             <article>
-                <img src={support} />
+                <img src={support} alt="wheel of fortune" />
                 <h1>We&rsquo;ll be back soon!</h1>
                 <div>
                     <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:#">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
